9d5bdea0c4efe2c056497d79d1af99b7dca9864b
Tiago Marin <tiago.lelinski@gmail.com>
2022-12-15 11:56:41 -0300

    install TailwindCss

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import './app.css';
 function App() {
   return (
     <div className="App">
-      <h1>Tiago Marin</h1>      
+      <h1 className="text-3xl font-bold underline">Tiago Marin</h1>      
     </div>
   );
 }
