03bffbaf88a7b8e62402e9cc4a629dbb8ed42885
Tiago Marin <tiago.lelinski@gmail.com>
2022-12-15 10:59:25 -0300

    install Parcel bundler and React

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tiago Marin</h1>      
+    </div>
+  );
+}
+
+export default App;
